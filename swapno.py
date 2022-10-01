b6e456e20f7957a0ce62335eb8f68a7603832f75
Shubham Rai <srai21029@gmail.com>
2022-10-01 16:40:23 +0530

    swapping number

---
+++ swapno.py
@@ -0,0 +1,14 @@
+#Take inputs from user for two numbers ( x and y )
+from re import X
+
+
+a= 5
+b= 10
+print("value of a is :" ,a)
+print("value of b is :" ,b)
+temp=a
+print("value of temp is:" ,a)
+a=b
+print("value of a is:" ,a)
+b=temp
+print("value of b is:" ,b)
