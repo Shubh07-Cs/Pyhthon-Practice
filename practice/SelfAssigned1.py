279142c21a7233565a0a4e9d1a38f8a3f0f0e520
Shubham Rai <srai21029@gmail.com>
2022-10-05 04:58:54 +0530

    assignment

---
+++ practice/SelfAssigned1.py
@@ -0,0 +1,35 @@
+#1Write a program to add two numbers
+from re import A
+
+
+a=4
+b=23456
+print("The sum of a and b is", a+b)
+#2Write a python program to find remainder when a number is divided by 7
+c=4567
+d=7
+print("The remainder when c is divided by d is", c%d)
+#3Check the type of the variable assigned using input () function.
+e=input("Write Anything")
+print(e)
+f=float(e)
+print(type(f))
+#4Use comparision operators to find out whether a is given a A is greater than B or not.
+A=25
+B=80
+print(A>B)
+print(B>A)
+print(A==B)
+print(B>=A)
+#5Write a python program to find average of two numbers entered by the user.
+g=input("Enter first no")
+h=input("Enter second no")
+g=int(g)
+h=int(h)
+avg=(g+h)/2
+print("The average of g and h is", avg)
+#6Write a python program to calculate square of a number entered by the user.
+i=input("Enter a no")
+i=int(i)
+sqr=i*i
+print("the square of the no is", sqr)
