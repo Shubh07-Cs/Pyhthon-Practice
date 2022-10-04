17ba5eaca256a611ad326d719ca497026404ebc0
Shubham Rai <srai21029@gmail.com>
2022-10-05 04:01:09 +0530

    self practice

---
+++ practice/Typecasting.py
@@ -0,0 +1,11 @@
+a="35"
+print(type(a))
+#we cant add 'a' with any integer since its a string so we need to typecast and change 'a' into interger class to add .
+b=int(a)
+print(type(b))
+print(b+34)
+c=25
+print(type(c))
+d=str(c)
+print(type(d))
+#"35" is a string literal and 35 is a numeric literal
