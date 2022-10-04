c7820eac90e31fb7abeba3c8d1589d97353ea38f
Shubham Rai <srai21029@gmail.com>
2022-10-04 23:48:26 +0530

    add

--- add.py
+++ add.py
@@ -1,8 +1,15 @@
 #Create two variables (x&y) and assign values to them
-x=99
-y=11
+x= input("enter first no :")
+y= input("enter second no :")
 #Create a third variable(sum) and add x & y
 sum=(x+y)
 print(sum)
 #print the sum
 print(x+y+sum)
+print(x)
+sum= int(x)+ int(y)
+print("the answer is : " , sum)
+z=1
+a=2
+
+print(z+a)
