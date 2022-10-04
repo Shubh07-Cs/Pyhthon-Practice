8914f90af5110ad864ee878fd06eb8af72889280
Shubham Rai <srai21029@gmail.com>
2022-10-05 03:46:33 +0530

    self practice

---
+++ practice/Types_of_operators.py
@@ -0,0 +1,19 @@
+a=34
+b=35
+#Arithmetic Operators(all operations using +,-,* and /)
+print("the value of 34+35 is",34+35)
+#Assignment Operators(shortcut for arithmetic operators)
+a += 2
+print(a)
+#Comarision Operator(used for t/f outputs)
+print(b>a)
+print(a>b)
+print(a>=b)
+print(a==b)
+print(a!=b)
+#Logical Oerators(not clear)
+bool1= True
+bool2= False
+print("The value of bool1 and bool2 is", bool1 and bool2)
+print("The value of bool1 and bool2 is", bool1 or bool2)
+print("The value of bool1 and bool2 is", not bool1)
