ad49969477635370e6cb2e8adb417e38c26e69c3
Shubham Rai <srai21029@gmail.com>
2022-10-10 22:14:41 +0530

    gone

---
+++ Comparision_between_two_nos.py
@@ -0,0 +1,10 @@
+# take 2 inputs from user (can be float)
+# orint the greatest no
+No1=float(input("enter a no"))
+No2=float(input("enter other no"))
+if No1>No2:
+    print("greatest no is", No1)
+elif No2>No1:
+    print("greatest no is", No2)    
+else:
+    print("Both nos are same")    
