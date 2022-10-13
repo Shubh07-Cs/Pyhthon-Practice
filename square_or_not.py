7ae0cedf4814ac85b9f54e4498e6a8e61cf340bf
Shubham Rai <srai21029@gmail.com>
2022-10-13 23:45:17 +0530

    play

---
+++ square_or_not.py
@@ -0,0 +1,8 @@
+#take values of length and breadth from user
+#print if it is a square or not
+Length=float(input("Length="))
+Breadth=float(input("breadth="))
+if Length==Breadth:
+    print("It is a square")
+else:
+    print("Not a square")
