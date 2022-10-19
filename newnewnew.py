90929a00494fc488c406e766751aed25d3abf574
Shubham Rai <srai21029@gmail.com>
2022-10-19 17:39:10 +0530

    practice

---
+++ newnewnew.py
@@ -0,0 +1,9 @@
+sentence="Hey There"
+print(sentence[:])
+print(sentence[-1:])
+story= "My name is champak."
+print(len(story))
+print(story[0:])
+print(story[:19])
+print(story[2:17])
+print(story[:])
