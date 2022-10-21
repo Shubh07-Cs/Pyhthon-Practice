aad90e70f4ff9f6a38073a35b68c79d05974062d
Shubham Rai <srai21029@gmail.com>
2022-10-21 22:00:38 +0530

    ding dong

---
+++ krrish.py
@@ -0,0 +1,9 @@
+a=input("put a vowel:")
+b=("a,e,i,o,u,A,E,I,O,U")
+c=("b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z,B,C,D,F,G,H,J,K,L,M,N,P,Q,R,S,T,V,W,X,Y,Z")
+if a in b:
+    print("GOOD BOY")
+elif a in c:
+    print("Almost there!")
+else:
+    print("BAD BOY")    
