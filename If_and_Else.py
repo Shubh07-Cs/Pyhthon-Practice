eb1f8b3f79a95708576bef854e623a4893601088
Shubham Rai <srai21029@gmail.com>
2022-12-01 22:24:13 +0530

    travailent

--- If_and_Else.py
+++ If_and_Else.py
@@ -1,17 +1,5 @@
-#a=b,a!=b,a>b,a<b,a>=b,a<=b
-a=5
-b=10
-if a<b:
-    print("b is greater than a")    
-
-else:
-    print("you are wrong")
-#Take user input  for x and y 
-# If x is less than y, print YAYY
-# else print NAYY   
-x=float(input("Enter a no:"))
-y=float(input("Enter a no:"))   
-if x<y:
-    print("YAYY")
-else:
-    print("NAYY")    
+a=input()
+i=0
+while a[i]<=len(a):
+    print(sum)
+    i=i+1
