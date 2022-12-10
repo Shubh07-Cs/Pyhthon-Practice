801c58afaefbb558900ec659bf695c27969b8139
Shubham Rai <srai21029@gmail.com>
2022-12-10 23:20:05 +0530

    loop

---
+++ practice/error.py
@@ -0,0 +1,132 @@
+                #Loop practice from PYnative.
+                #---------------------------#
+
+
+# for i in range(1,11):
+#     print(i)
+
+
+
+# k=' '
+# for i in range(1,7):
+#     for j in range(1,i):
+#         print(j,end=' ')
+#     print('')
+
+
+
+# k=0
+# a=int(input())
+# for i in range(1,a+1):
+#     k+=i
+# print(k)    
+
+
+
+# a=int(input())
+# k=1
+# for i in range(1,11):
+#     k=i*a
+#     print(k)
+
+
+
+# num=[12,75,150,180,145,525,50]
+# for i in num:
+#     if i>500:
+#         break
+#     elif i>150:
+#         continue
+#     elif i%5==0:
+#         print(i)  
+
+
+
+# a=int(input())
+# a=str(a)
+# print(len(a))
+#Using while, count the total no of digits in a number
+
+
+
+# list1=[10,20,30,40,50]
+# list1.reverse()
+# for i in list1:
+#     print(i) 
+
+
+
+# for i in range(-10,0):
+#     print(i)
+
+
+
+# for i in range(5):
+#     print(i)
+# print("Done!")    
+
+
+
+# print("Print numbers between 25 and 50 are:")
+# for i in range(25,50):  
+#     if i%2!=0 and i%3!=0 and i%5!=0 and i%7!=0:
+#         print(i)
+
+
+
+#Fibonacci sequence
+# k=0
+# l=1
+# for i in range(10):
+#     print(k, end=" ")
+#     m=k+l
+#     k=l
+#     l=m
+
+
+
+# a=int(input())
+# k=1
+# if a==0 and a==1:
+#     print(1) 
+# else:
+#     for i in range(1,a+1):
+#         k=k*i
+#     print(k)    
+
+
+
+# a=int(input())
+# a=str(a)
+# print(a[::-1])
+# reverse a no using while function
+
+
+
+# my_list=[10,20,30,40,50,60,70,80,90,100]
+# for i in my_list:
+#     if i%20==0:
+#         print(i,end=" ")
+
+
+
+# a=int(input())
+# for i in range(1,a+1):
+#     print("Current Number is :",i,"and the cube is",i**3)
+
+
+
+# a=2
+# n=int(input())
+# k=0
+# for i in range(0,n):
+#     print(a, end="+")
+#     k=k+a
+#     a=a*10+2
+# print(k)    
+
+
+
+#print * n times and slow down it.
+
+    
