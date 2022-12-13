461de9782dbf35e060e3b4f0d564d6ee462dd008
Shubham Rai <srai21029@gmail.com>
2022-12-13 23:27:14 +0530

    tuple

---
+++ practice/tupleprac2.py
@@ -0,0 +1,11 @@
+# a=input()
+# b=input()
+# c=input()
+# d=input()
+# e=input()
+# set=(a,b,c,d,e)
+# print(set)
+
+name1,name2,name3,name4,name5=input(": "),input(": "),input(": "),input(": "),input(": ")
+f=(name1,name2,name3,name4,name5)
+print(f)
