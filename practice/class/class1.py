1b756a69153ae2838d98ecaef9e047feb6832241
Shubham Rai <srai21029@gmail.com>
2022-12-17 22:04:10 +0530

    class1

---
+++ practice/class/class1.py
@@ -0,0 +1,30 @@
+class Laptop:
+    #attributes behaviour
+    def config(self):
+        print("i7","1tB","16GB")
+
+laptop1=Laptop()       
+# Laptop.config() 
+laptop1.config()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
