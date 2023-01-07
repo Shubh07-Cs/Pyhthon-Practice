a1b2e17c3cba396c9002d2bc49d31b983e7629aa
Shubh07-Cs <srai21029@gmail.com>
2023-01-07 22:11:23 +0530

    decoraters

--- practice/Decorators.py
+++ practice/Decorators.py
@@ -1,11 +1,11 @@
 #if we want to change file that is immutable, we use decorators.
-def div(a,b):
+def division(a,b):
     print(a/b)
-def new_div(func):
+def new_division(func):
     def innerfunc(a,b):
         if a<b:
             a,b=b,a
         return func(a,b)
     return innerfunc            
-div=new_div(div)
-div(4,8)    
+div=new_division(division)
+division(4,8)    
