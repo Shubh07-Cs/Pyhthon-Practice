409ab2c848a0f9acde227351b11f41961b7eb140
Shubh07-Cs <srai21029@gmail.com>
2023-01-21 23:57:17 +0530

    js loop

---
+++ javascript.py
@@ -0,0 +1,18 @@
+<script>
+    // for (i=0; i<50; i++){
+    //     if (i%2!=0 && i%3!=0 ){
+    //         console.log(i)
+    //     }
+    // }
+    // let year=prompt("Write a year:");
+    // year=Number.parseInt(year);
+    // if (year%4==0){
+    //     console.log("Its a leap yr")
+    // }
+    // else{
+    //     console.log("Not a leap year")
+    // }
+    
+   
+
+</script>
